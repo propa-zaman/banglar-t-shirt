84dedf33024ae7e691ee06319edf59c25e79fe06
Afra <propazaman12@gmail.com>
2023-04-07 21:38:01 +0600

    context api

--- src/Component/Grandpa/Grandpa.jsx
+++ src/Component/Grandpa/Grandpa.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Aunty from '../Aunty/Aunty';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css'
 
+ // 1. create context
+export const RingContext = createContext('gold');
+export const MoneyContext = createContext(0);
+
 const Grandpa = () => {
     const ring = 'diamond';
+    const [money, setMoney] = useState(0);
+
+   
+
     return (
         <div className='grandpa'>
             <h2>Grandpa</h2>
-            <section className='flex'>
+            <p>has money{money}</p>
+            {/* 2. set a provider and paass a value */}
+           <MoneyContext.Provider value={[money, setMoney]}>
+           <RingContext.Provider value='golden ring'>
+           <section className='flex'>
                 
-                    <Father ring={ring}></Father>
-                    <Uncle></Uncle>
-                    <Aunty></Aunty>
-                
-            </section>
+                <Father ring={ring}></Father>
+                <Uncle></Uncle>
+                <Aunty></Aunty>
+            
+        </section>
+           </RingContext.Provider>
+           </MoneyContext.Provider>
         </div>
     );
 };
 
 export default Grandpa;
+
+/****
+ * 1. create context and export
+ * 2. create a provider and pass a value
+ * 3. use useContext to receive the value
+ */
--- src/Component/Uncle/Uncle.jsx
+++ src/Component/Uncle/Uncle.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Cousin from '../Cousin/Cousin';
+import { MoneyContext } from '../Grandpa/Grandpa';
 
 const Uncle = () => {
+    const [money, setMoney] = useContext(MoneyContext);
     return (
         <div>
             <h2>Uncle</h2>
+            <p>grandpa money: {money}</p>
+            <button onClick={() => setMoney(money + 1000)}>Send 1000tk</button>
             <section className='flex'>
                 <Cousin>Nibir</Cousin>
                 <Cousin>Abir</Cousin>
